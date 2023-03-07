0b2ed43ffb78116b4eed6cf2a7e645d7d0132a40
byeollim woo <byeollim.woo@gmail.com>
2023-03-07 12:00:12 +0900

    commit

--- src/test/java/com/greedy/crud/model/service/MenuServiceTests.java
+++ src/test/java/com/greedy/crud/model/service/MenuServiceTests.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
